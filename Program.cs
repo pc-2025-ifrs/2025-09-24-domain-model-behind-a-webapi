0015ce11f0a7a1b38428684d64f85d28ed88788e
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2025-10-26 23:49:49 -0300

    video

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
+using trabalho.Repositories;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//add repositories 
+builder.Services.AddSingleton<ICanalRepository, InMemoryCanalRepository>();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
+builder.Services.AddSwagger();
+builder.Services.AddEndPointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.MapOpenApi();
+    app.MapSwagger();
+    app.MapEndPointsApiExplorer();
 }
 
 app.UseHttpsRedirection();
